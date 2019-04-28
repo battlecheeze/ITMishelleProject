967b9899ecfaa04b94a13267dc7731d8c147d6a4
Christian Albert Provido <39144431+battlecheeze@users.noreply.github.com>
2019-04-29 02:36:16 +0800

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -24,3 +24,54 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// Mouse over the btns
+
+var btnWorkArray = document.getElementsByClassName('btn-work');
+var btnTeamArray = document.getElementsByClassName('btn-team');
+
+btnWorkArray[0].addEventListener('mouseover', addColorTeam);
+btnWorkArray[0].addEventListener('mouseout', removeColorTeam);
+
+function addColorTeam() {
+  btnTeamArray[0].classList.add('team-color');
+}
+
+function removeColorTeam() {
+  btnTeamArray[0].classList.remove('team-color');
+}
+
+
+btnTeamArray[0].addEventListener('mouseover', addColorWork);
+btnTeamArray[0].addEventListener('mouseout', removeColorWork);
+
+function addColorWork() {
+  btnWorkArray[0].classList.add('work-color');
+} 
+
+function removeColorWork() {
+  btnWorkArray[0].classList.remove('work-color');
+}
+
+btnWorkArray[1].addEventListener('mouseover', addColorTeam);
+btnWorkArray[1].addEventListener('mouseout', removeColorTeam);
+
+function addColorTeam() {
+  btnTeamArray[1].classList.add('team-color');
+}
+
+function removeColorTeam() {
+  btnTeamArray[1].classList.remove('team-color');
+}
+
+
+btnTeamArray[1].addEventListener('mouseover', addColorWork);
+btnTeamArray[1].addEventListener('mouseout', removeColorWork);
+
+function addColorWork() {
+  btnWorkArray[1].classList.add('work-color');
+} 
+
+function removeColorWork() {
+  btnWorkArray[1].classList.remove('work-color');
+}
